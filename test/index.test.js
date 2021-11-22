8b2711b907afd079f24b11aeaba7515cf0d5118e
Antoine Boileau <antoine@boileau.io>
2021-11-22 14:46:13 +0100

    ADD a correction

--- test/index.test.js
+++ test/index.test.js
@@ -1,7 +1,6 @@
 const { expect, sinon, faker } = require('./testHelper')
 const dependency = require('../lib/dependency')
 const tpModule = require('../lib/index')
-const { fi } = require('faker/lib/locales')
 
 beforeEach(() => {
   sinon.stub(dependency, 'getRandomNumber')
@@ -28,8 +27,7 @@ describe('question 1 : multiplier par deux', () => {
 // Question 2
 // Faire en sorte de renvoyer le nombre reçu en argument en l'additionnant avec
 // celui récupéré par l'appel getRandomNumber
-// soit retourné multiplié par deux
-describe.skip('question 2 : additionner deux nombres', () => {
+describe('question 2 : additionner deux nombres', () => {
   let number1
   let number2
 
@@ -48,7 +46,7 @@ describe.skip('question 2 : additionner deux nombres', () => {
 
 // Question 3
 // Faire en sorte de renvoyer 0 en cas d'erreur
-describe.skip('question 3 : gérer les cas d’erreur', () => {
+describe('question 3 : gérer les cas d’erreur', () => {
   beforeEach(() => {
     dependency.getRandomNumber.rejects(new Error('Number generation failed'))
   })
@@ -61,22 +59,22 @@ describe.skip('question 3 : gérer les cas d’erreur', () => {
 // Question 4
 // Faire en sorte de lever une erreur si le nombre est impair
 // et de renvoyer le nombre s'il est pair
-describe.skip('question 4 : générer des cas d’erreur', () => {
+describe('question 4 : générer des cas d’erreur', () => {
   let oddNumber
   let evenNumber
   beforeEach(() => {
     // un nombre aléatoire entre 0 et 100
     oddNumber = Math.floor(Math.random() * 50) * 2 + 1
-    evenNumber = Math.floor(Math.random() * 50)
+    evenNumber = Math.floor(Math.random() * 50) * 2
   })
 
-  it.skip('la fonction renvoie le nombre si pair', () => {
+  it('la fonction renvoie le nombre si pair', () => {
     dependency.getRandomNumber.resolves(evenNumber)
 
-    return expect(tpModule.getEvenNumbers()).to.eventually.be.equal(0)
+    return expect(tpModule.getEvenNumbers()).to.eventually.be.equal(evenNumber)
   })
 
-  it.skip('la fonction retourne une erreur si le nombre est impair', () => {
+  it('la fonction retourne une erreur si le nombre est impair', () => {
     dependency.getRandomNumber.resolves(oddNumber)
 
     return expect(tpModule.getEvenNumbers()).to.eventually.be.rejected
@@ -87,7 +85,7 @@ describe.skip('question 4 : générer des cas d’erreur', () => {
 // Faire en de récupérer un prénom plus un nombre pour générer un
 // pseudo sous la forme {{prénom}}-{{nombre}}
 // par exemple tom-48
-describe.skip('question 5 : gérer plusieurs appels asynchrones', () => {
+describe('question 5 : gérer plusieurs appels asynchrones', () => {
   let firstname
   let number
   beforeEach(() => {
@@ -109,21 +107,21 @@ describe.skip('question 5 : gérer plusieurs appels asynchrones', () => {
 // Question 6
 // Faire en de récupérer un prénom plus un nombre pour générer un
 // pseudo sous la forme {{prénom}}-{{nombre}}
-// par exemple tom-48
+// par exemple mathias-48
 // Attention :
 // - gérer le cas d'erreur de getRandomNumber en mettant 0
 //   comme nombre si il y a une erreur à la génération du nombre
-// - gérer le cas d'erreur de getUserPseudo en mettant "Bob"
+// - gérer le cas d'erreur de getUserPseudo en mettant "Jérome"
 //   comme pseudo si il y a une erreur à la génération de pseudo
-// - lever une erreur si la longeur du pseudo est plus petite (ou égal) que 6
+// - lever une erreur si la longueur du pseudo est plus petite (ou égal) que 6
 //   bob-0 --> trop court (length = 5) thomas-23 --> ok (length = 9)
-describe.skip('question 6 : cas complet', () => {
+describe('question 6 : cas complet', () => {
   let firstname
   let number
 
-  it.skip('la fonction retourne une pseudo généré', () => {
-    // un prénom aléatoire
-    firstname = faker.name.firstName()
+  it('la fonction retourne un pseudo généré', () => {
+    // un prénom aléatoire de plus de 5 char
+    firstname = `${faker.name.firstName()}...`
     // un nombre aléatoire entre 0 et 100
     number = Math.floor(Math.random() * 100)
     dependency.getUserName.resolves(firstname)
@@ -134,9 +132,9 @@ describe.skip('question 6 : cas complet', () => {
     return expect(tpModule.getUserPseudo()).to.eventually.be.equal(expectedPseudo)
   })
 
-  it.skip('la fonction utilise 0 si erreur durant la génération d’un nombre', () => {
-    // un prénom aléatoire
-    firstname = faker.name.firstName()
+  it('la fonction utilise 0 si erreur durant la génération d’un nombre', () => {
+    // un prénom aléatoire de plus de 5 char
+    firstname = `${faker.name.firstName()}...`
     dependency.getUserName.resolves(firstname)
     dependency.getRandomNumber.rejects(new Error('Number generation failed'))
 
@@ -145,18 +143,19 @@ describe.skip('question 6 : cas complet', () => {
     return expect(tpModule.getUserPseudoFull()).to.eventually.be.equal(expectedPseudo)
   })
 
-  it.skip('la fonction utilise Bob si erreur durant la génération d’un prénom', () => {
+  it('la fonction utilise Jérôme si erreur durant la génération d’un prénom', () => {
     // un prénom aléatoire
     number = Math.floor(Math.random() * 100)
     dependency.getUserName.rejects(new Error('Firstname generation failed'))
     dependency.getRandomNumber.resolves(number)
 
-    const expectedPseudo = `Bob-${number}`
+    const expectedPseudo = `Jérôme-${number}`
 
     return expect(tpModule.getUserPseudoFull()).to.eventually.be.equal(expectedPseudo)
   })
 
-  it.skip('la fonction retourne une erreur si le pseudo est trop court', () => {
+  it('la fonction retourne une erreur si le pseudo est trop court', () => {
+    // un prénom aléatoire de seulement 4 char
     firstname = 'John'
     number = 2
     dependency.getUserName.resolves(firstname)
